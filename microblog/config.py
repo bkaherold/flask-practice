2ec7d489daf93ca4af690628f586c3e8052b93c6
Brienna Herold <brienna.herold@gmail.com>
2015-04-16 10:44:50 -0400

    Updated config.py with database configs, updated __init__.py to initialize database

--- microblog/config.py
+++ microblog/config.py
@@ -1,10 +1,21 @@
+#database configurations
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+
 #settings FLASK-WTF extension needs
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+#openid providers
 OPENID_PROVIDERS = [
 	{'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
 	{'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
 	{'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
 	{'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
 ]
+
